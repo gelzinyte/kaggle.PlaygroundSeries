dc8150fb4fd34452573c7bda82879814df2cffbd
gelzinyte <elena.gelzinyte@gmail.com>
2025-05-24 20:05:11 +0200

    simpler ols

---
+++ models/ols_simpler.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import logging
+
+from sklearn.preprocessing import StandardScaler
+from sklearn import linear_model
+from sklearn.metrics import root_mean_squared_error, root_mean_squared_log_error
+
+import util
+
+util.setup_logger()
+
+
+def main():
+
+    target_col = "Calories"
+
+    df_test = pd.read_csv("../../data/test.csv", index_col="id")
+    df_train = pd.read_csv("../../data/train.csv", index_col="id")
+
+    print("train", df_train.describe())
+    print("unique", df_train.nunique())
+    print("test", df_test.describe())
+    print("unique", df_test.nunique())
+
+    df = pd.concat([df_train, df_test])
+    df = pd.get_dummies(df)
+    y_raw = df.pop(target_col)
+    y_log = y_raw.apply(lambda x: np.log(x + 1))
+
+    ys_train = y_log[y_log.notna()]
+    Xs_train = df[y_log.notna()]
+
+    ols = linear_model.LinearRegression()
+    model = ols.fit(Xs_train, ys_train)
+
+    ys_pred_log = model.predict(df)
+    ys_pred_log = pd.Series(ys_pred_log, index=df.index)
+    ys_pred = np.exp(ys_pred_log) - 1
+
+    ys_pred_train = ys_pred[y_raw.notna()]
+    ys_ref_train = y_raw[y_raw.notna()]
+
+    util.plot_correlation(reference=ys_ref_train, predicted=ys_pred_train)
+
+    if np.min(ys_pred_train) < 0:
+        logging.error
+        (
+            f"min y pred train: {np.min(ys_pred_train)}, can't log for error metric"
+        )
+
+    ys_pred_test = ys_pred[y_raw.isna()]
+    ys_pred_test.to_csv("submission.csv")
+
+
+if __name__ == "__main__":
+    logging.info(
+        "fitting directly to dummied scaled shifted features gives negative predictions"
+    )
+    logging.info("ofc scaling or not scaling doesn't make a difference here")
+    main()
---
+++ models/util.py
@@ -0,0 +1,55 @@
+import logging
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from sklearn.metrics import root_mean_squared_error
+
+def setup_logger():
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO) 
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+
+
+def adjust_ax(ax):
+    min_x, max_x = ax.get_xlim()
+    min_y, max_y = ax.get_ylim()
+
+    min_lim = np.min([min_x, min_y])
+    max_lim = np.max([max_x, max_y])
+
+    ax.plot([min_lim, max_lim], [min_lim, max_lim], color="k", lw=1)
+
+    ax.set_ylim(min_lim, max_lim)
+    ax.set_xlim(min_lim, max_lim)
+    plt.legend() 
+
+
+def plot_correlation(reference, predicted, out_fn="correlation.png"):
+
+    fig = plt.figure(figsize=(5, 5))
+    gs = gridspec.GridSpec(1, 1)
+
+    ax = fig.add_subplot(gs[0, 0])
+    rmse = root_mean_squared_error(reference, predicted)
+    print(f"rmse: {rmse}")
+    ax.scatter(reference, predicted, label=f"RMSE {rmse:.3f}")
+    ax.set_ylabel("Prediction")
+    ax.set_xlabel("Reference")
+    ax.set_title("RMSE")
+    adjust_ax(ax)
+    
+    
+    plt.tight_layout()
+    plt.savefig(out_fn, dpi=300)
+
+
+ 
+
